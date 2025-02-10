99754c506191b56371925bfe6f2eae07431dce52
Musa Musa Kannike <156730039+musamusakannike@users.noreply.github.com>
2025-02-10 07:48:03 +0100

    Added refresh token and logout to route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { signup, login } = require("../controllers/auth.controller");
+const { signup, login, refreshAccessToken, logout } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/refresh-token", refreshAccessToken);
+router.post("/logout", logout);
 
 module.exports = router;
